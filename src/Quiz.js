56be0f7fd4dea3fa920dd141385ce8a762a04ced
Ilia <riawork94@gmail.com>
2020-07-23 00:38:53 +0300

    upgrade quiz foundaition

--- src/Quiz.js
+++ src/Quiz.js
@@ -4,14 +4,20 @@ import './App.css';
 import Demon_sprite from './data/spr/Demon_sprite.png'
 
 function Quiz() {
-    return (
-        <body className = "Quiz-foundation">
-            <img
-                src={Demon_sprite}
-            />
-        </body>
-    )
-
+  return (
+    <div className="Quiz-foundation">
+      <div className='Sprite-box'>
+        <img
+          src={Demon_sprite} style={{backgroundSize: 'cover', height: '100%'}}>
+        </img>
+      </div>
+        <div className ="Button-panel">
+          <button className='Button-sound'>Sound 1</button>
+          <button className='Button-sound'>Sound 2</button>
+          <button className='Button-sound'>Sound 3</button>
+      </div>
+    </div>
+  )
 }
 
 export default Quiz;
