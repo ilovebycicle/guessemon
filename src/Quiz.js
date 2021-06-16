4e097ab4ffeb2ddfabc6c8aea945b4d458b4b8d3
Sergey Spasskiy <greyyar93@mail.ru>
2021-06-16 21:11:48 +0300

    Add correctSound prop, fix highlight logic & small improvements

--- src/Quiz.js
+++ src/Quiz.js
@@ -9,19 +9,21 @@ function Quiz(props) {
   const [isDisabledButtons, setIsDisabledButtons] = useState(false);
   const [hurlAcceleration, setHurlAcceleration] = useState(0);
 
-  const correctAnswer = 2; // Здесь хардкод правильного ответа
+  const correctAnswer = props.correctSound; // Здесь хардкод правильного ответа
+  const generalVolume = 0.45;
+  const delayBetweenQuestionsMs = 1500;
 
-// play1-2-3 объединить как-то
+  // play1-2-3 объединить как-то
 
   const [play1] = useSound(
     props.sounds[0],
-    { volume: 0.25, interrupt: true, onend: () => setIsDisabledButtons(false) }
+    { volume: generalVolume, interrupt: true, onend: () => setIsDisabledButtons(false) }
   );
 
   const [play2] = useSound(
     props.sounds[1],
     {
-      volume: 0.25,
+      volume: generalVolume,
       playbackRate: (props.sounds[1].includes('dssgtatk') ? 0.8 + hurlAcceleration : 1), // Здесь код пасхалки, если звук хухурл то он ускоряется
       interrupt: true, onend: () => setIsDisabledButtons(false)
     }
@@ -29,27 +31,30 @@ function Quiz(props) {
 
   const [play3] = useSound(
     props.sounds[2],
-    { volume: 0.25, interrupt: true, onend: () => setIsDisabledButtons(false) }
+    { volume: generalVolume, interrupt: true, onend: () => setIsDisabledButtons(false) }
   );
 
   const optionButtonClassName = (optionButtonNumber) => {
-    if (optionButtonNumber == selectedButton) {
+    if (optionButtonNumber === selectedButton) {
       if (isAnswered) {
         return (
-          optionButtonNumber == correctAnswer ? 'Button-sound Selected-button Right-answer' : 'Button-sound Selected-button Wrong-answer'
+          optionButtonNumber === correctAnswer ?
+            'Button-sound Selected-button Right-answer' :
+            'Button-sound Selected-button Wrong-answer'
         )
       }
-      return(
+      return (
         'Button-sound Selected-button'
       )
     }
-    return(
+    return (
       'Button-sound'
     )
   }
 
   const buttonHandling = (soundToPlay, number) => {
-    soundToPlay == play2 && props.sounds[1].includes('dssgtatk') && setHurlAcceleration(hurlAcceleration + 0.1);
+    soundToPlay === play2 && props.sounds[1].includes('dssgtatk') &&
+      setHurlAcceleration(hurlAcceleration + 0.1);
     setSelectedButton(number);
     setIsDisabledButtons(true);
     soundToPlay();
@@ -57,9 +62,12 @@ function Quiz(props) {
 
   const continueHandling = () => {
     setIsAnswered(true);
-    setTimeout(setIsAnswered, 3000, false);
+    setTimeout(() => {
+      setIsAnswered(false);
+      props.toggleNext();
+      setSelectedButton(0);
+    }, delayBetweenQuestionsMs);
     setHurlAcceleration(0);
-    props.toggleNext();
   }
 
   return (
@@ -70,12 +78,36 @@ function Quiz(props) {
         </img>
       </div>
       <div className="Button-panel">
-        <button className={optionButtonClassName(1)} onClick={() => buttonHandling(play1, 1)} disabled={isAnswered || isDisabledButtons}>Sound №1</button>
-        <button className={optionButtonClassName(2)} onClick={() => buttonHandling(play2, 2)} disabled={isAnswered || isDisabledButtons}>Sound №2</button>
-        <button className={optionButtonClassName(3)} onClick={() => buttonHandling(play3, 3)} disabled={isAnswered || isDisabledButtons}>Sound №3</button>
+        <button
+          className={optionButtonClassName(1)}
+          onClick={() => buttonHandling(play1, 1)}
+          disabled={isAnswered || isDisabledButtons}
+        >
+          Sound №1
+        </button>
+        <button
+          className={optionButtonClassName(2)}
+          onClick={() => buttonHandling(play2, 2)}
+          disabled={isAnswered || isDisabledButtons}
+        >
+          Sound №2
+        </button>
+        <button
+          className={optionButtonClassName(3)}
+          onClick={() => buttonHandling(play3, 3)}
+          disabled={isAnswered || isDisabledButtons}
+        >
+          Sound №3
+        </button>
       </div>
       <div className="Button-panel">
-        <button className='Button-sound' disabled={selectedButton == 0 || isAnswered} onClick={continueHandling} >Continue</button>
+        <button
+          className='Button-sound'
+          disabled={selectedButton === 0 || isAnswered}
+          onClick={continueHandling}
+        >
+          Continue
+        </button>
       </div>
     </div>
   )
